16fc9309fe381fb8694654ea94d478c9c5888b5c
Wes Young <wesley.young@du.edu>
2021-02-03 11:07:50 -0700

    Seed data populating

---
+++ models/Workout.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const WorkoutSchema = new mongoose.Schema({
+  day: { type: Date },
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
